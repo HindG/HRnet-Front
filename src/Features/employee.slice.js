98223415d7856005f8dcc0818830b94ce2e9f13b
Hind <guessoushind@gmail.com>
2023-02-10 13:19:24 +0100

    Correct slice

--- src/Features/employee.slice.js
+++ src/Features/employee.slice.js
@@ -1,15 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = {
-  lastname: "",
-  firstname: "",
-  birthDate: "",
-  startDate: "",
-  streetAddress: "",
-  cityAddress: "",
-  stateAddress: "",
-  zipCodeAddresse: 0,
-  department: "",
+  employeesList: [],
 }
 
 const employeeSlice = createSlice({
@@ -18,15 +10,18 @@ const employeeSlice = createSlice({
   reducers: {
     create: (state, action) => ({
       ...state,
-      lastname: action.payload[0],
-      firstname: action.payload[1],
-      birthDate: action.payload[2],
-      startDate: action.payload[3],
-      streetAddress: action.payload[4],
-      cityAddress: action.payload[5],
-      stateAddress: action.payload[6],
-      zipCodeAddresse: action.payload[7],
-      department: action.payload[8],
+      employeesList: state.employeesList.push(
+        {
+          lastname: action.payload[0],
+          firstname: action.payload[1],
+          birthDate: action.payload[2],
+          startDate: action.payload[3],
+          streetAddress: action.payload[4],
+          cityAddress: action.payload[5],
+          stateAddress: action.payload[6],
+          zipCodeAddresse: action.payload[7],
+          department: action.payload[8],
+        })
     }),
   },
 })
