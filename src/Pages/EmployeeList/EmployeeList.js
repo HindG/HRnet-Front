9c16a20b870a9067c2192b1ae048c300e211b077
Hind <guessoushind@gmail.com>
2023-04-21 16:39:34 +0200

    Add pagination

--- src/Pages/EmployeeList/EmployeeList.js
+++ src/Pages/EmployeeList/EmployeeList.js
@@ -77,15 +77,25 @@ function EmployeeList() {
 
             setFilteredArray(employeeArray)
         }
+        sePageNumber(1)
     }
 
     function handlepageSizeClick(value) {
         sepageSize(value)
         setDisplayDropdown(!displayDropdown)
+        sePageNumber(1)
     }
 
     function handleDropDown() {
         setDisplayDropdown(!displayDropdown)
+    }
+
+    function getPageNumbers() {
+        let paginationArray = []
+        for (let i = 1; i <= Math.ceil(filteredArray.length / pageSize); i++) {
+            paginationArray.push(<div onClick={() => sePageNumber(i)}>{i}</div>)
+        }
+        return paginationArray
     }
 
 
@@ -157,6 +167,7 @@ function EmployeeList() {
                     <div>Showing {(pageNumber - 1) * pageSize + 1} to {pageSize < employeeArray.length ? pageSize : employeeArray.length} of {employeeArray.length} entries</div>
                     <div className="dataTables_paginate paging_simple_numbers" id="employee-table_paginate">
                         <div className="paginate_button previous disabled">Previous</div>
+                        {getPageNumbers()}
                         <div className="paginate_button previous disabled">Next</div>
                     </div>
                 </div>
